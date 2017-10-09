616f776ef6525ac471e1f1326a47cc8aaeb4ef6c
S <sharnam19.nc@gmail.com>
2017-10-09 12:24:15 +0530

    Model Training

--- NN.py
+++ NN.py
@@ -144,8 +144,6 @@ class NN:
     
     
     def train(self,X,y):
-        self.J=[]
-        self.accuracies=[]
         self.test(X,y)
         print("Initial Cost :"+str(self.J[-1]))
         print("Initial Accuracy :"+str(self.accuracies[-1]))
@@ -202,21 +200,21 @@ class NN:
         plt.show()
         
 if __name__== "__main__":
-    model = NN(input_shape=(64,1,50,50),update_params={'alpha':1e-6,'method':'adam','epoch':10,'offset':1e-7,'reg':0.01,'reg_type':'L2'},initialization="xavier2")
+    model = NN(input_shape=(64,1,50,50),update_params={'alpha':1e-3,'method':'adam','epoch':100,'offset':1e-7,'reg':0.01,'reg_type':'L2'},initialization="xavier2")
     model.add("padding",padding_h=2,padding_w=2)
     model.add("convolution",num_kernels=64,kernel_h=3,kernel_w=3,convolution_params={"stride":1})
     model.add("pooling",pooling_params={"pooling_height":2,"pooling_stride_height":2,
                                         "pooling_stride_height":2,"pooling_stride_height":2})
     model.add("relu")
-    #model.add("convolution",num_kernels=128,kernel_h=3,kernel_w=3,convolution_params={"stride":1})
-    '''model.add("pooling",pooling_params={"pooling_height":2,"pooling_stride_height":2,
+    model.add("convolution",num_kernels=128,kernel_h=3,kernel_w=3,convolution_params={"stride":1})
+    model.add("pooling",pooling_params={"pooling_height":2,"pooling_stride_height":2,
                                         "pooling_stride_height":2,"pooling_stride_height":2})
-    '''
-    #model.add("relu")
+    
+    model.add("relu")
     model.add("flatten")
-    #model.add("affine",affine_out=128)
-    #model.add("affine",affine_out=64)
-    #model.add("affine",affine_out=16)
+    model.add("affine",affine_out=128)
+    model.add("affine",affine_out=64)
+    model.add("affine",affine_out=16)
     model.add("affine",affine_out=5)
     model.add("softmax")
     
