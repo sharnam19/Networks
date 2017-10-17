d97ddb455fe517cf6226c070f9a9a9796413846b
S <sharnam19.nc@gmail.com>
2017-10-17 20:25:20 +0530

    Setup.py v0.3.3

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'networks',
     packages = ['networks','networks/layers','networks/layers/util/','networks/layers/descent/'],
-    version = '0.3.1',  # Ideally should be same as your GitHub release tag varsion
+    version = '0.3.2',  # Ideally should be same as your GitHub release tag varsion
     description = 'Allows to create NN models',
     author = 'Shharrnam Chhatpar',
     author_email = 'sharnam19.nc@gmail.com',
-    url = 'https://github.com/sharnam19/NN-Library',
-    download_url = 'https://github.com/sharnam19/NN-Library/archive/0.3.1.tar.gz',
+    url = 'https://github.com/sharnam19/Networks',
+    download_url = 'https://github.com/sharnam19/Networks/archive/v0.3.3.tar.gz',
     keywords = ['machine-learning', 'deep-learning','neural-networks','linear regression',
                 'logistic regression'],
     classifiers = [],
